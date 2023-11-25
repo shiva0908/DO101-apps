73da1b42452f8289a325790a30c33636d29a3b47
shiva0908 <pathipakashiva8@gmail.com>
2023-11-25 12:00:44 +0530

    add/mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/',function (reg,res) {
+  res.send('hello mars!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
